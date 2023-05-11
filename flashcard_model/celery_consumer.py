65dd7e1a4fd78b582cd7ea446a5d8a602a7a2c4a
SimonEndres <66074656+SimonEndres@users.noreply.github.com>
2023-05-11 19:38:14 +0200

    first draft

--- flashcard_model/celery_consumer.py
+++ flashcard_model/celery_consumer.py
@@ -2,6 +2,7 @@ from celery import Celery
 
 from flashcard_model.DemoModel import DemoModel
 from flashcard_model.database_functions import load_extracted_pages
+from flashcard_model.flashcard_model.ChatGPTModel import ChatGPTModel
 
 app = Celery('flashcard_model')
 app.conf.task_default_queue = "flashcard_model"
@@ -12,6 +13,6 @@ app.conf.accept_content = ["json", "pickle"]
 @app.task(name="generate_flashcards")
 def generate_flashcard(extraction_result):
     extracted_pages = load_extracted_pages(extraction_result["document_id"])
-    demo_model = DemoModel()
+    demo_model = ChatGPTModel()
     demo_model(extraction_result['document_id'], extracted_pages)
     print(f"Document {extraction_result['document_id']} flashcard generation finished")
